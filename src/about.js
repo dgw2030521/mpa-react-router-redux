bb7a081d98c3b9fa0eab11f6b251253efc280100
guowei.dong <112166729+dgw2030521@users.noreply.github.com>
2025-04-04 19:14:38 +0800

    修改

--- src/about.js
+++ src/about.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, createBrowserRouter, Route, RouterProvider, Routes} from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 import routeConfig from './routes';
 import About from './pages/About';
 
 
+const router = createBrowserRouter(routeConfig);
+
+
 const root = ReactDOM.createRoot(document.getElementById('about'));
 root.render(
   <Provider store={store}>
-    <Router>
+    {/*<Router>*/}
+    {/*<PersistGate loading={null} persistor={persistor}>*/}
+    {/*  <Routes>*/}
+    {/*    <Route path="/about" element={<About />} />*/}
+    {/*  </Routes>*/}
+    {/*</PersistGate>*/}
+    {/*</Router>*/}
+
     <PersistGate loading={null} persistor={persistor}>
-      <Routes>
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </PersistGate></Router>
+      <RouterProvider router={router} />
+    </PersistGate>
+
+
   </Provider>,
   document.getElementById('root')
 );
--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, createBrowserRouter, Route, RouterProvider, Routes} from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 import routeConfig from './routes';
 import Home from './pages/Home';
 
+const router = createBrowserRouter(routeConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <Router>
+    {/*<Router>*/}
+    {/*  <PersistGate loading={null} persistor={persistor}>*/}
+    {/*    <Routes>*/}
+    {/*      <Route path="/" element={<Home />} />*/}
+    {/*    </Routes>*/}
+    {/*  </PersistGate>*/}
+    {/*</Router>*/}
+
       <PersistGate loading={null} persistor={persistor}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <RouterProvider router={router} />
       </PersistGate>
-    </Router>
   </Provider>
 );
